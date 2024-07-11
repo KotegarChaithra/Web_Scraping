442dd0147a5cb1d24b47a9015a57f4154d53906a
KotegarChaithra <155987898+KotegarChaithra@users.noreply.github.com>
2024-07-11 13:26:12 +0530

    Add files via upload

--- element_by_expath.py
+++ element_by_expath.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 s = Service("C:/Users/hp/Documents/softwares/chromedriver-win64/chromedriver.exe")
 driver = webdriver.Chrome(service = s)
 
 driver.get("https://www.wscubetech.com/")
-driver.find_element_by_xpath("""/html/body/section[4]/div/div/div[3]/div""")# path expression which gives path of any element
+driver.find_element(By.XPATH,"""/html/body/section[4]/div/div/div[3]/div""")# path expression which gives path of any element
 
-driver.find_element_by_xpath("""/html/body/section[1]/div[2]/div/div[1]/div[2]/div/button""")
+driver.find_element(By.XPATH,"""/html/body/section[1]/div[2]/div/div[1]/div[2]/div/button""")
